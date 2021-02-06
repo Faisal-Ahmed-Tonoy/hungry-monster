a1aca253eabf4f4a3f727b7c24e704cfa3748957
Faisal-Ahmed-Tonoy <71918750+Faisal-Ahmed-Tonoy@users.noreply.github.com>
2021-02-07 03:00:29 +0600

    fetch ok

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.getElementById('search-button').addEventListener('click', function() {
 
 
+
+
     const mealInput = document.getElementById("meal-input").value;
-    Url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=${mealInput}'
+    const Url = (`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealInput}`)
     fetch(Url)
         .then(response => response.json())
         .then(data => console.log(data))
