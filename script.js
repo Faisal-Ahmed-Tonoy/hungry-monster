51dffbb842d0916380f4faefe1a3ea4b7a620620
Faisal-Ahmed-Tonoy <71918750+Faisal-Ahmed-Tonoy@users.noreply.github.com>
2021-02-07 04:53:53 +0600

    font

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 document.getElementById('search-button').addEventListener('click', function() {
-
-
-
-
     const mealInput = document.getElementById("meal-input").value;
     const Url = (`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealInput}`)
     fetch(Url)
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => displayMeal(data))
 })
