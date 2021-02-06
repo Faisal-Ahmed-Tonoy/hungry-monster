90d62887f4b8e5532d63e682537751309f4d08d1
Faisal-Ahmed-Tonoy <71918750+Faisal-Ahmed-Tonoy@users.noreply.github.com>
2021-02-07 02:45:19 +0600

    fetch ok

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-document.getElementById('search_button').addEventListener('click', function() {
-    const mealInput = document.getElementById('meal-input').value;
-    const Url = `https://www.themealdb.com/api/json/v1/1/search.php?${mealInput}`
+document.getElementById('search-button').addEventListener('click', function() {
 
+
+    const mealInput = document.getElementById("meal-input").value;
+    Url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=${mealInput}'
     fetch(Url)
         .then(response => response.json())
-        .then(data => getWeather(data))
+        .then(data => console.log(data))
 })
