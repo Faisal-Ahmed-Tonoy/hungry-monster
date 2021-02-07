6c253bed5a931c9581497efc83d8b63235ead53a
Faisal-Ahmed-Tonoy <71918750+Faisal-Ahmed-Tonoy@users.noreply.github.com>
2021-02-07 19:16:59 +0600

    finished3

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //API Call
 document.getElementById('search-button').addEventListener('click', function() {
         const mealInput = document.getElementById("meal-input").value;
-        const Url = (`https://www.themealdb.com/api/json/v1/1/random.php${mealInput}`)
+        const Url = (`https://www.themealdb.com/api/json/v1/1/search.php?f=${mealInput}`)
         fetch(Url)
             .then(response => response.json())
             .then(data => displayMeal(data.meals))
