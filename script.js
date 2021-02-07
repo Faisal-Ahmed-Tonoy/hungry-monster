d5f753aec511afb3220675c4d7e5e30da1d62ce2
Faisal-Ahmed-Tonoy <71918750+Faisal-Ahmed-Tonoy@users.noreply.github.com>
2021-02-07 06:35:21 +0600

    found array

--- script.js
+++ script.js
@@ -3,5 +3,17 @@ document.getElementById('search-button').addEventListener('click', function() {
     const Url = (`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealInput}`)
     fetch(Url)
         .then(response => response.json())
-        .then(data => displayMeal(data))
+        .then(data => displayMeal(data.meals))
 })
+
+const displayMeal = meal => {
+    for (let i = 0; i < meal.length; i++) {
+        const meals = meal[i];
+        console.log(meals.strCategory);
+
+
+
+    }
+
+
+}
